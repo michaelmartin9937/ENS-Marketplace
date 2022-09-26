24281d4b72533ffa3f9aece804d29032764dcbc8
Michael Martin <michaelmartin@Michaels-MacBook-Pro.local>
2022-09-25 21:37:35 -0700

    1. Upload ENS Marketplace

---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
+
+const privateKey = process.env.PRIVATE_KEY || "";
+
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`${privateKey}`]
+    }
+  },
+  paths: {
+    artifacts: "./src/backend/artifacts",
+    sources: "./src/backend/contracts",
+    cache: "./src/backend/cache",
+    tests: "./src/backend/test"
+  },
+};
